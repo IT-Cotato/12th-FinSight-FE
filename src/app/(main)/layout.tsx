ee6397c509d680e59d832bbc0d1757bc190a8508
chaewon41 <chaew0m3x@gmail.com>
2026-01-04 13:48:14 +0900

    feat: app-container 클래스 제거

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -6,7 +6,7 @@ export default function MainLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-col min-h-screen max-w-md mx-auto w-full shadow-lg bg-[#131416]">
+    <div className="flex flex-col min-h-screen shadow-lg">
       <main className="flex-1 overflow-y-auto pb-16">{children}</main>
       <BottomTab />
     </div>

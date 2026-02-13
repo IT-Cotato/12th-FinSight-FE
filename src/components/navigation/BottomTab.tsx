01678694fb85121e3f47f6768e3e772334738f85
chaewon41 <chaew0m3x@gmail.com>
2026-02-13 19:29:16 +0900

    style: 바텀탭 아이콘 수정

--- src/components/navigation/BottomTab.tsx
+++ src/components/navigation/BottomTab.tsx
@@ -9,6 +9,7 @@ type BottomTabItem = {
   label: string;
   href: string;
   icon: string;
+  activeIcon: string;
 };
 
 const TAB_ITEMS: BottomTabItem[] = [
@@ -16,25 +17,29 @@ const TAB_ITEMS: BottomTabItem[] = [
     key: "home",
     label: "홈",
     href: "/home",
-    icon: "/bottomTab/Home.svg",
+    icon: "/bottomTab/icon-home.svg",
+    activeIcon: "/bottomTab/icon-home-active.svg",
   },
   {
     key: "study",
     label: "학습",
     href: "/study",
-    icon: "/bottomTab/Study.svg",
+    icon: "/bottomTab/icon-study.svg",
+    activeIcon: "/bottomTab/icon-study-active.svg",
   },
   {
     key: "archive",
     label: "보관함",
     href: "/archive",
-    icon: "/bottomTab/Archive.svg",
+    icon: "/bottomTab/icon-archive.svg",
+    activeIcon: "/bottomTab/icon-archive-active.svg",
   },
   {
     key: "mypage",
     label: "마이페이지",
     href: "/mypage",
-    icon: "/bottomTab/mypage.svg",
+    icon: "/bottomTab/icon-mypage.svg",
+    activeIcon: "/bottomTab/icon-mypage-active.svg",
   },
 ];
 
@@ -57,14 +62,12 @@ export function BottomTab() {
               className="flex flex-col items-center justify-center transition-colors"
             >
               <Image
-                src={item.icon}
+                src={isActive ? item.activeIcon : item.icon}
                 alt={item.label}
                 width={24}
                 height={24}
                 className={`h-6 w-6 transition-all ${
-                  isActive
-                    ? "brightness-0 invert"
-                    : "opacity-60"
+                  isActive ? "" : "opacity-60"
                 }`}
               />
               <span className={`text-[10px] mt-1 transition-colors ${

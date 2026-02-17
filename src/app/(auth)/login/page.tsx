29d532a796e154d3158295eee8743fd90141e785
sxxbxxny <gyg031@naver.com>
2026-02-17 10:21:01 +0900

    충돌 해결2

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -4,11 +4,7 @@ import Image from 'next/image';
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import AuthContainer from '../AuthContainer';
-<<<<<<< Updated upstream
-import { login } from '@/api/auth'; // auth.ts의 login 함수 import
-=======
 import { login } from '@/api/auth';
->>>>>>> Stashed changes
 
 
 export default function LoginPage() {

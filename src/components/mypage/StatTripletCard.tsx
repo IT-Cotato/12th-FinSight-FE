d0092beb61da73f77fb16395db1f695f96a370d0
jeongbam <bellajw292@gmail.com>
2026-02-18 14:10:58 +0900

    refactor: 마이페이지 피드백 반영

--- src/components/mypage/StatTripletCard.tsx
+++ src/components/mypage/StatTripletCard.tsx
@@ -22,7 +22,7 @@ export default function StatTripletCard({
     <section className="mt-6 px-5">
       <div className="h-[90px] rounded-xl border border-primary-90 bg-primary-100 px-3">
         <div className="grid h-full grid-cols-[1fr_auto_1fr_auto_1fr] items-center">
-          <Stat label="저장한 뉴스" value={`${attendanceDays}일`} />
+          <Stat label="저장한 뉴스" value={`${attendanceDays}개`} />
           <div className="mx-auto h-10 w-px bg-primary-90" />
           <Stat label="뉴스 저장" value={`${totalNewsSaved}개`} />
           <div className="mx-auto h-10 w-px bg-primary-90" />
